0b5b3bdaa108bac70b5e740d29620d768634e7ee
João Vitor <jvde.olivera@gmail.com>
2025-05-07 02:17:15 -0300

    Tela Play - Pronta

--- app/src/main/java/com/example/segundoaplicativo/Musicas.kt
+++ app/src/main/java/com/example/segundoaplicativo/Musicas.kt
@@ -1,6 +1,8 @@
 package com.example.segundoaplicativo
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +11,14 @@ import androidx.core.view.WindowInsetsCompat
 class Musicas : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_musicas)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+    }
+
+    fun btnArtistas(view:View){
+        var intent = Intent(this, AtividadeAppSpotify::class.java)
+        startActivity(intent)
+        overridePendingTransition(0, 0) // Remove a animação
+
     }
 }
